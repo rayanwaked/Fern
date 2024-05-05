eddd52723ff9aba561f84220eb1bb059eed16cf4
Rayan Waked <rayan@waked.dev>
2024-05-05 16:12:38 -0700

    Initial Commit

---
+++ Fern/FernApp.swift
@@ -0,0 +1,17 @@
+//
+//  FernApp.swift
+//  Fern
+//
+//  Created by Rayan Waked on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FernApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
